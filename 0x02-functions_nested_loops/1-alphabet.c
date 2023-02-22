a527a4199ca00377fa973eb2218213a0029abaec
solerock <propexticy8@gmail.com>
2023-02-22 00:11:29 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet: function that prints lowercase alphabet
+ * print_alphabet - function that prints lowercase alphabet
  *
- * Return: Always 0:
+ * Return: Always 0.
  */
-
 
 void print_alphabet(void)
 {
