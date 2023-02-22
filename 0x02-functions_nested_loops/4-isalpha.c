a1525ef5dbab32c15e9b847c6240e6f2affbe3e3
solerock <propexticy8@gmail.com>
2023-02-22 10:41:03 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to print the alphabet.
+ * @c : parameter to be printed on the screen
+ *
+ * Return: 1 if it's an alphabet
+ * and 0 otherwise
+ **/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
