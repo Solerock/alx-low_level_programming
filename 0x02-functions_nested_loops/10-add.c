a121100b3f04e483ef79d3a42249a6bcb733a675
solerock <propexticy8@gmail.com>
2023-02-22 14:37:22 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - function to print sum value
+ * @a : parameter_1 to be printed on the screen
+ * @b : parameter_2 to be printed on the screen
+ * Return: a always
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
